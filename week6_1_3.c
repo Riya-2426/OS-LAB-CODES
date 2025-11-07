1c5cfc529c843ef3f931733e56c6045346faa99f
Riya-2426 <riyarawat740@gmail.com>
2025-11-07 20:59:00 +0530

    shared memory

---
+++ week6_1_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    char *str = (char*) shmat(shmid, NULL, 0);
+
+    if (fork() == 0) {
+        strcpy(str, "Hello from child");
+    } else {
+        sleep(1);
+        printf("Parent received: %s\n", str);
+        shmdt(str);
+        shmctl(shmid, IPC_RMID, NULL);
+    }
+    return 0;
+}
